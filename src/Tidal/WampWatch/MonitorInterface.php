c9857d5acf8b08e5cb1ff953d7ea3cd2fca42b03
Timo Michna <timomichna@yahoo.de>
2016-06-26 15:38:49 +0200

    added MonitorInterface

---
+++ src/Tidal/WampWatch/MonitorInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tidal\WampWatch;
+
+/*
+ * Copyright 2016 Timo Michna.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+use Evenement\EventEmitterInterface;
+
+/**
+ *
+ * @author Timo Michna
+ */
+interface MonitorInterface {
+    
+    public function start();
+
+    public function stop();
+    
+}
