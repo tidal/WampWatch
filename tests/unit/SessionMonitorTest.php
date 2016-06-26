6eddfa4b42f75ce9878a48019bcdfa5009225af4
Timo Michna <timomichna@yahoo.de>
2016-06-26 22:37:12 +0200

    initial start test with mock

--- tests/unit/SessionMonitorTest.php
+++ tests/unit/SessionMonitorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/stub/ClientSessionStub.php';
 
 use Mockery as M;
 use Tidal\WampWatch\SessionMonitor;
@@ -13,8 +14,31 @@ class SessionMonitorTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-
+ 
+    }
+    
+    public function tearDown()
+    {
+         M::close();
     }
 
+    public function test_start() {
+        $this->session = $this->getMockBuilder('\Thruway\ClientSession')
+                ->disableOriginalConstructor()
+                ->getMock();
+        $futureResult = new \React\Promise\Deferred();
+        $this->session->expects($this->any())
+            ->method('call')
+            ->will(
+                $this->returnValue($futureResult->promise())
+            );
+        
+        $this->monitor = new SessionMonitor($this->session);
+        $this->monitor->start();
+        
+       
+        
+    }
+    
   
 }
