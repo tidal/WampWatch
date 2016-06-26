0d6d80b05077a7b29cc5279d60444b2d6c446d73
Timo Michna <timomichna@yahoo.de>
2016-06-26 16:55:16 +0200

    add initial SessionMonitorTest to trigger travis

---
+++ tests/unit/SessionMonitorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+use Mockery as M;
+use Tidal\WampWatch\SessionMonitor;
+
+
+/**
+ * @author Timo Michna <timomichna@yahoo.de>
+ */
+class SessionMonitorTest extends M\Adapter\PHPUnit\MockeryTestCase
+{
+    public function setUp()
+    {
+
+    }
+
+  
+}
