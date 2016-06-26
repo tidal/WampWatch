bc4da59efe90c59f0549b6314d9d6e2f5a389ebc
Timo Michna <timomichna@yahoo.de>
2016-06-26 16:41:41 +0200

    add initial test bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+ini_set("xdebug.max_nesting_level","200");
+if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    $loader = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
