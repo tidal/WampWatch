28bccf71ff01717cd1c7d7c5dc6e7da6b9156932
Timo Michna <timomichna@yahoo.de>
2016-06-26 18:29:23 +0200

    added Util.php

---
+++ src/Tidal/WampWatch/Util.php
@@ -0,0 +1,34 @@
+<?php
+namespace Tidal\WampWatch;
+
+/*
+ * Copyright 2016 Timo Michna.
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+use Thruway\Common\Utils;
+use Thruway\ClientSession;
+use Thruway\Message\UnsubscribedMessage;
+use React\Promise\Deferred;
+
+/**
+ * Utility Class for the WampWatch package
+ *
+ * @author Timo Michna
+ */
+class Util {
+    
+    
+    public static function unsubscribe(ClientSession $session, $topic) {
+        $requestId = Utils::getUniqueId();
+        $deferred  = new Deferred();
+        $unsubscribeMsg = new UnsubscribedMessage($requestId);
+        $session->sendMessage($unsubscribeMsg);
+        
+        return $deferred->promise();
+    }
+    
+    
+}
