80cc140f18ba00d4f11b169d0d6f50528255f6d3
Timo Michna <timomichna@yahoo.de>
2016-07-10 01:41:01 +0200

    added Thruway ClientSession Adapter

---
+++ src/Tidal/WampWatch/Adapter/Thruway/ClientSession.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tidal\WampWatch\Adapter\Thruway;
+
+/*
+ * This file is part of the Tidal/WampWatch package.
+ * (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Thruway\ClientSession as ThruwaySession;
+use Tidal\WampWatch\ClientSessionInterface;
+
+
+class ClientSession extends ThruwaySession implements ClientSessionInterface
+{
+    
+}
