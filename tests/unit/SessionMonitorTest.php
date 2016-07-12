1f3792f993183c51df6617ceebaf9cfe12106d08
Timo Michna <timomichna@yahoo.de>
2016-07-12 16:08:53 +0200

    added test_start_calls_session_list

--- tests/unit/SessionMonitorTest.php
+++ tests/unit/SessionMonitorTest.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__.'/../bootstrap.php';
-//require_once __DIR__ . '/stub/ClientSessionStub.php';
+
 
 use Mockery as M;
 use Tidal\WampWatch\Adapter\Thruway\ClientSession;
@@ -47,9 +47,12 @@ class SessionMonitorTest extends PHPUnit_Framework_TestCase
         $session = M::mock('Thruway\ClientSession');
         $session->shouldReceive('call')->once()
             ->andReturn($promise);
+
+        // test 'wamp.session.on_join' was called.
         $session->shouldReceive('subscribe')->once()
             ->with('wamp.session.on_join', \Mockery::any(), \Mockery::any())
             ->andReturn($promise);
+        // test 'wamp.session.on_leave' was called.
         $session->shouldReceive('subscribe')->once()
             ->with('wamp.session.on_leave', \Mockery::any(), \Mockery::any())
             ->andReturn($promise);
@@ -58,4 +61,29 @@ class SessionMonitorTest extends PHPUnit_Framework_TestCase
         $monitor = new SessionMonitor($adapter);
         $monitor->start();
     }
+
+    public function test_start_calls_session_list()
+    {
+        $promise = M::mock('React\Promise\Promise');
+        $promise->shouldReceive('then')->atLeast()->once();
+
+        $session = M::mock('Thruway\ClientSession');
+        $session->shouldReceive('subscribe')->once()
+            ->andReturn($promise);
+        $session->shouldReceive('subscribe')->once()
+            ->andReturn($promise);
+
+        // test 'wamp.session.list' was called.
+        $session->shouldReceive('call')->once()
+            ->with('wamp.session.list', \Mockery::any(), \Mockery::any(), \Mockery::any())
+            ->andReturn($promise);
+
+        $adapter = new ClientSession($session);
+        $monitor = new SessionMonitor($adapter);
+        $monitor->start();
+    }
+
+
+
+
 }
