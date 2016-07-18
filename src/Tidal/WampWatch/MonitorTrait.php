f12bf9c74e5b65688f3ca9e307b6f19081b72116
Timo Michna <timomichna@yahoo.de>
2016-07-18 02:40:53 +0200

    monitor trait refactoring part 1

--- src/Tidal/WampWatch/MonitorTrait.php
+++ src/Tidal/WampWatch/MonitorTrait.php
@@ -12,7 +12,9 @@
 namespace Tidal\WampWatch;
 
 use Evenement\EventEmitterTrait;
+use React\Promise\Promise;
 use Tidal\WampWatch\ClientSessionInterface as ClientSession;
+use Tidal\WampWatch\Subscription\Collection as SubscriptionCollection;
 
 /**
  * Description of MonitorTrait.
@@ -36,6 +38,26 @@ trait MonitorTrait
      * @var bool
      */
     protected $isRunning = false;
+
+    /**
+     * @var SubscriptionCollection
+     */
+    protected $subscriptionCollection;
+
+    /**
+     * @var string
+     */
+    protected $initialCallProcedure;
+
+    /**
+     * @var callable
+     */
+    protected $initialCallCallback;
+
+    /**
+     * @var bool
+     */
+    protected $initialCallDone = false;
 
     /**
      * @param ClientSession $session
@@ -69,6 +91,9 @@ trait MonitorTrait
         if (!$this->isRunning()) {
             return false;
         }
+
+        $this->getSubscriptionCollection()->unsubscribe();
+
         $this->isRunning = false;
         $this->emit('stop', [$this]);
 
@@ -98,5 +123,54 @@ trait MonitorTrait
     public function isRunning()
     {
         return $this->isRunning;
+    }
+
+    /**
+     * @return \Tidal\WampWatch\Subscription\Collection
+     */
+    protected function getSubscriptionCollection()
+    {
+        return isset($this->subscriptionCollection)
+            ? $this->subscriptionCollection
+            : $this->subscriptionCollection = new SubscriptionCollection($this->session);
+    }
+
+    protected function setInitialCall($pocedure, callable $callback)
+    {
+        $this->initialCallProcedure = (string)$pocedure;
+        $this->initialCallCallback = $callback;
+    }
+
+    /**
+     * @return \React\Promise\PromiseInterface
+     */
+    protected function callInitialProcedure()
+    {
+        if (!isset($this->initialCallProcedure) || !isset($this->initialCallCallback)) {
+            $resolver = function (callable $resolve) {
+                $resolve();
+            };
+
+            return new  Promise($resolver);
+        }
+
+        return $this->session->call($this->initialCallProcedure)->then(function ($res) {
+            $this->initialCallDone = true;
+
+            return $res;
+        });
+    }
+
+    /**
+     * Checks if all necessary subscriptions and calls have been responded to.
+     */
+    protected function checkStarted()
+    {
+        if ($this->getSubscriptionCollection()->isSubscribed() &&
+            $this->initialCallDone &&
+            !$this->isRunning()
+        ) {
+            $this->doStart();
+        }
     }
 }
