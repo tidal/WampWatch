4a0cd822905eb6bc59921d33c9d876ca3f625821
Timo Michna <timomichna@yahoo.de>
2016-07-21 00:06:16 +0200

    more tests

--- tests/unit/MonitorTraitTest.php
+++ tests/unit/MonitorTraitTest.php
@@ -3,7 +3,9 @@
 require_once __DIR__.'/../bootstrap.php';
 require_once __DIR__.'/stub/MonitorTraitImplementation.php';
 
-use \Tidal\WampWatch\Adapter\Thruway\ClientSession;
+use Tidal\WampWatch\Adapter\Thruway\ClientSession;
+use Tidal\WampWatch\Stub\ClientSessionStub;
+use Tidal\WampWatch\Subscription\Collection;
 use Mockery as M;
 
 /**
@@ -19,61 +21,248 @@ class MonitorTraitTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->mock = M::mock('\Thruway\ClientSession');
-        $this->session = new ClientSession($this->mock);
-        $this->monitorStub = new MonitorTraitImplementation($this->session);
         $this->lastEvent = '';
         $this->lastArgs = [];
     }
 
     public function test_get_server_session()
     {
-        $this->assertEquals($this->session, $this->monitorStub->getServerSession());
+
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $this->assertEquals($stub, $monitor->getServerSession());
     }
 
     public function test_start()
     {
-        $testCase = $this;
-        $this->monitorStub->on('start', function ($list) use ($testCase) {
-            $testCase->lastEvent = 'start';
-            $testCase->lastArgs = func_get_args();
+        $lastEvent = '';
+        $lastArgs = [];
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $monitor->on('start', function () use (&$lastEvent, &$lastArgs) {
+            $lastEvent = 'start';
+            $lastArgs = func_get_args();
         });
 
-        $res = $this->monitorStub->start();
+        $res = $monitor->start();
 
         $this->assertSame(true, $res);
-        $this->assertSame('start', $this->lastEvent);
-        $this->assertSame([], $this->lastArgs[0]);
-        $this->assertSame(true, $this->monitorStub->isRunning(), 'monitor should be running, when it was started');
+        $this->assertSame('start', $lastEvent);
+        $this->assertTrue(is_array($lastArgs));
+        $this->assertSame(true, $monitor->isRunning(), 'monitor should be running, when it was started');
     }
 
     public function test_stop_without_being_started()
     {
-        $testCase = $this;
-        $this->monitorStub->on('stop', function ($list) use ($testCase) {
-            $testCase->lastEvent = 'stop';
-            $testCase->lastArgs = func_get_args();
+        $lastEvent = '';
+        $lastArgs = [];
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $monitor->on('stop', function () use (&$lastEvent, &$lastArgs) {
+            $lastEvent = 'start';
+            $lastArgs = func_get_args();
         });
-        $res = $this->monitorStub->stop();
+        $res = $monitor->stop();
 
         $this->assertSame(false, $res, "'stop' should return false, when monitor was not started");
-        $this->assertSame('', $this->lastEvent, "'stop' event should not trigger, when monitor was not started");
+        $this->assertSame('', $lastEvent, "'stop' event should not trigger, when monitor was not started");
     }
 
     public function test_stop_with_being_started()
     {
-        $testCase = $this;
-        $this->monitorStub->on('stop', function ($list) use ($testCase) {
-            $testCase->lastEvent = 'stop';
-            $testCase->lastArgs = func_get_args();
+        $lastEvent = '';
+        $lastArgs = [];
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $monitor->on('stop', function () use (&$lastEvent, &$lastArgs) {
+            $lastEvent = 'stop';
+            $lastArgs = func_get_args();
         });
 
-        $this->monitorStub->start();
-        $res = $this->monitorStub->stop();
+        $monitor->start();
+        $res = $monitor->stop();
 
         $this->assertSame(true, $res, "'stop' should return true, when monitor was stopped");
-        $this->assertSame('stop', $this->lastEvent, "'stop' event should not trigger, when monitor was not started");
-        $this->assertSame($this->monitorStub, $this->lastArgs[0]);
-        $this->assertSame(false, $this->monitorStub->isRunning(), 'monitor should not be running, when it was stopped');
+        $this->assertSame('stop', $lastEvent, "'stop' event should not trigger, when monitor was not started");
+        $this->assertSame($monitor, $lastArgs[0]);
+        $this->assertSame(false, $monitor->isRunning(), 'monitor should not be running, when it was stopped');
+    }
+
+
+    public function test_starts_returns_true()
+    {
+
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $res = $monitor->start();
+
+        $this->assertEquals(true, $res);
+    }
+
+    public function test_is_not_running_before_started()
+    {
+
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+
+        $this->assertFalse($monitor->isRunning());
+    }
+
+    public function test_is_not_running_before_meta_subscriptions()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+        $monitor->start();
+
+        $stub->respondToCall('bar', [[321]]);
+
+        $this->assertFalse($monitor->isRunning());
+    }
+
+
+    public function test_is_not_running_before_initial_call()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+        $monitor->start();
+
+        $stub->completeSubscription('foo');
+
+        $this->assertFalse($monitor->isRunning());
+    }
+
+    public function test_is_running_after_subscriptions_and_list()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+        $monitor->start();
+
+        $stub->completeSubscription('foo');
+        $stub->respondToCall('bar', [[321]]);
+
+        $this->assertTrue($monitor->isRunning());
+    }
+
+    public function test_start_event_after_running()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+
+        $called = null;
+
+        $monitor->on('start', function () use (&$called) {
+            $called = true;
+        });
+
+        $monitor->start();
+
+        $stub->completeSubscription('foo');
+        $stub->respondToCall('bar', [[321]]);
+
+        $this->assertTrue($called);
+    }
+
+    // SUBSCRIPTION AND CALL TESTS
+
+    public function test_start_subscribes_to_topic()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+        $monitor->start();
+
+        $this->assertTrue(
+            $stub->hasSubscription('foo')
+        );
+
+    }
+
+    public function test_start_calls_procedure()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+        $monitor->start();
+
+        $this->assertTrue(
+            $stub->hasCall('bar')
+        );
+
+    }
+
+
+    // STOP MONITOR TESTS
+
+
+    public function test_is_not_running_after_stop()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+
+        $monitor->start();
+
+        $stub->completeSubscription('foo');
+        $stub->respondToCall('bar', [[321]]);
+
+        $monitor->stop();
+
+        $this->assertFalse($monitor->isRunning());
+    }
+
+    public function test_stop_emits_event()
+    {
+        $stub = new ClientSessionStub();
+        $monitor = new MonitorTraitImplementation($stub);
+        $collection = new Collection($stub);
+        $collection->addSubscription('foo', $this->getEmptyFunc());
+        $monitor->setMetaSubscriptionCollection($collection);
+        $monitor->setInitialCall('bar', $this->getEmptyFunc());
+
+        $monitor->start();
+
+        $response = null;
+
+        $monitor->on('stop', function ($res) use (&$response) {
+            $response = $res;
+        });
+
+        $stub->completeSubscription('foo');
+        $stub->respondToCall('bar', [[321]]);
+
+        $monitor->stop();
+
+        $this->assertSame($monitor, $response);
+    }
+
+    private function getEmptyFunc()
+    {
+        return function () {
+        };
     }
 }
