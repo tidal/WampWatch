8de5840e2750aef0f57a760d69730d028a72c45b
Timo Michna <timomichna@yahoo.de>
2016-09-28 00:59:27 +0200

    added PromiseStub

---
+++ tests/unit/Stub/PromiseStub.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ *  * This file is part of the Tidal/WampWatch package.
+ *  * (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *  *
+ *  * For the full copyright and license information, please view the LICENSE
+ *  * file that was distributed with this source code.
+ *
+ */
+
+use Tidal\WampWatch\Async\PromiseInterface;
+
+class PromiseStub implements PromiseInterface
+{
+    /**
+     * @param callable|null $onFulfilled
+     * @param callable|null $onRejected
+     * @param callable|null $onProgress
+     *
+     * @return self
+     */
+    public function then(callable $onFulfilled = null, callable $onRejected = null, callable $onProgress = null)
+    {
+        return $this;
+    }
+
+    /**
+     * @param callable|null $onFulfilled
+     * @param callable|null $onRejected
+     * @param callable|null $onProgress
+     *
+     * @return self
+     */
+    public function done(callable $onFulfilled = null, callable $onRejected = null, callable $onProgress = null)
+    {
+        return $this;
+    }
+
+    /**
+     * @param callable $onRejected
+     *
+     * @return self
+     */
+    public function otherwise(callable $onRejected)
+    {
+        return $this;
+    }
+
+    /**
+     * @param callable $onFulfilledOrRejected
+     *
+     * @return self
+     */
+    public function always(callable $onFulfilledOrRejected)
+    {
+        return $this;
+    }
+
+    /**
+     * @param callable $onProgress
+     *
+     * @return self
+     */
+    public function progress(callable $onProgress)
+    {
+        return $this;
+    }
+}
