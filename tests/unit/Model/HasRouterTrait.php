59236e91b71d3e067e09daeec0c50805b204ff4e
Timo Michna <timomichna@yahoo.de>
2016-10-02 14:34:32 +0200

    added HasRouterTrait

---
+++ tests/unit/Model/HasRouterTrait.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Tidal/WampWatch package.
+ *  (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Tidal\WampWatch\Test\Unit\Model;
+
+use Tidal\WampWatch\Model\Router;
+use Tidal\WampWatch\Test\Unit\Stub\MockHelper;
+use PHPUnit_Framework_TestCase as TestCase;
+
+trait HasRouterTrait
+{
+    /**
+     * @param string $uri
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|Router
+     */
+    private function getRouterMock($uri = 'faz')
+    {
+        /** @var TestCase $self */
+        $self = $this;
+        $mock = MockHelper::getNoConstructorMock($self, Router::class);
+        $mock->expects($this->any())
+            ->method('getUri')
+            ->willReturn($uri);
+
+        return $mock;
+    }
+}
