6e1e9757b9716aeb61b14b6fc8a6aa43e0161b4e
Timo Michna <timomichna@yahoo.de>
2016-10-02 14:11:13 +0200

    added HasTopicTrait

---
+++ tests/unit/Model/HasTopicTrait.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * This file is part of the Tidal/WampWatch package.
+ * (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace Tidal\WampWatch\Test\Unit\Model;
+
+use Tidal\WampWatch\Model\Topic;
+use Tidal\WampWatch\Test\Unit\Stub\MockHelper;
+use PHPUnit_Framework_TestCase as TestCase;
+
+trait HasTopicTrait
+{
+    /**
+     * @param string $uri
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|Topic
+     */
+    private function getTopicMock($uri = 'foo')
+    {
+        /** @var TestCase $self */
+        $self = $this;
+        $mock = MockHelper::getNoConstructorMock($self, Topic::class);
+        $mock->expects($this->any())
+            ->method('getUri')
+            ->willReturn($uri);
+
+        return $mock;
+    }
+}
