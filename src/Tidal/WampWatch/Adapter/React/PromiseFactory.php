85434516c1e0b82afe9f605a9b4254a5d8b918c8
Timo Michna <timomichna@yahoo.de>
2016-10-03 21:11:17 +0200

    added PromiseFactory

---
+++ src/Tidal/WampWatch/Adapter/React/PromiseFactory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the Tidal/WampWatch package.
+ *  (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Tidal\WampWatch\Adapter\React;
+
+use Tidal\WampWatch\Async\Adapter\PromiseFactoryInterface;
+use React\Promise\Promise as ReactPromise;
+
+class PromiseFactory implements PromiseFactoryInterface
+{
+    /**
+     * @param callable      $resolver
+     * @param callable|null $canceller
+     *
+     * @return PromiseAdapter
+     */
+    public function create(callable $resolver, callable $canceller = null)
+    {
+        return $this->createFromAdaptee(
+            new ReactPromise(
+                $resolver,
+                $canceller
+            )
+        );
+    }
+
+    /**
+     * @param ReactPromise $adaptee
+     * @return PromiseAdapter
+     */
+    public function createFromAdaptee($adaptee)
+    {
+        return new PromiseAdapter($adaptee);
+    }
+}
