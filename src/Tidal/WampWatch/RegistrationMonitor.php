148b314f155fe53891a8281a93886ea9ceb4e6b1
Timo Michna <timomichna@yahoo.de>
2017-07-25 15:38:54 +0200

    add basic RegistrationMonitor

---
+++ src/Tidal/WampWatch/RegistrationMonitor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the Tidal/WampWatch package.
+ *  (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Tidal\WampWatch;
+
+/**
+ * Class Tidal\WampWatch\RegistrationMonitor *
+ */
+class RegistrationMonitor
+{
+    use MonitorTrait;
+
+    const REGISTRATION_CREATE_TOPIC = 'wamp.registration.on_create';
+    const REGISTRATION_REG_TOPIC = 'wamp.registration.on_register';
+    const REGISTRATION_UNREG_TOPIC = 'wamp.registration.on_unregister';
+    const REGISTRATION_DELETE_TOPIC = 'wamp.registration.on_delete';
+    const REGISTRATION_LIST_TOPIC = 'wamp.registration.list';
+    const REGISTRATION_LOOKUP_TOPIC = 'wamp.registration.lookup';
+    const REGISTRATION_MATCH_TOPIC = 'wamp.registration.match';
+    const REGISTRATION_GET_TOPIC = 'wamp.registration.get';
+    const REGISTRATION_REGLIST_TOPIC = 'wamp.registration.list_callees';
+    const REGISTRATION_REGCOUNT_TOPIC = 'wamp.registration.count_callees';
+}
