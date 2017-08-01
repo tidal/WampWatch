9e7707571eed099a5665aedbe7ab98e364548a87
Timo Michna <timomichna@yahoo.de>
2017-08-01 16:31:12 +0200

    add test_create_event

--- tests/unit/RegistrationMonitorTest.php
+++ tests/unit/RegistrationMonitorTest.php
@@ -199,17 +199,17 @@ class RegistrationMonitorTest extends \PHPUnit_Framework_TestCase
 
         $this->monitor->start();
 
-        $this->sessionStub->respondToCall(SubscriptionMonitor::SUBSCRIPTION_LIST_TOPIC, $subIdMap);
-        $this->sessionStub->completeSubscription(SubscriptionMonitor::SUBSCRIPTION_DELETE_TOPIC);
-        $this->sessionStub->completeSubscription(SubscriptionMonitor::SUBSCRIPTION_CREATE_TOPIC);
-        $this->sessionStub->completeSubscription(SubscriptionMonitor::SUBSCRIPTION_SUB_TOPIC);
-        $this->sessionStub->completeSubscription(SubscriptionMonitor::SUBSCRIPTION_UNSUB_TOPIC);
+        $this->sessionStub->respondToCall(RegistrationMonitor::REGISTRATION_LIST_TOPIC, $subIdMap);
+        $this->sessionStub->completeSubscription(RegistrationMonitor::REGISTRATION_DELETE_TOPIC);
+        $this->sessionStub->completeSubscription(RegistrationMonitor::REGISTRATION_CREATE_TOPIC);
+        $this->sessionStub->completeSubscription(RegistrationMonitor::REGISTRATION_REG_TOPIC);
+        $this->sessionStub->completeSubscription(RegistrationMonitor::REGISTRATION_UNREG_TOPIC);
 
         $this->monitor->on('create', function ($sessionId, $subscriptionInfo) use (&$res) {
             $res = [$sessionId, $subscriptionInfo];
         });
 
-        $this->sessionStub->emit(SubscriptionMonitor::SUBSCRIPTION_CREATE_TOPIC, [$info]);
+        $this->sessionStub->emit(RegistrationMonitor::REGISTRATION_CREATE_TOPIC, [$info]);
 
         $this->assertSame($info, $res);
     }
