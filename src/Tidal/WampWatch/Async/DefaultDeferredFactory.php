b7f34100920820d96f1d140522d32f45b4729763
Timo Michna <timomichna@yahoo.de>
2017-08-03 18:10:31 +0200

    add/use MakesPromisesTrait

--- src/Tidal/WampWatch/Async/DefaultDeferredFactory.php
+++ src/Tidal/WampWatch/Async/DefaultDeferredFactory.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of the Tidal/WampWatch package.
- *  (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *   (c) 2016 Timo Michna <timomichna/yahoo.de>
  *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
@@ -12,7 +12,7 @@ namespace Tidal\WampWatch\Async;
 use Tidal\WampWatch\Adapter\React\DeferredFactory;
 
 /**
- * Class Tidal\WampWatch\Async\DefaultDeferredFactory *
+ * Class Tidal\WampWatch\Async\DefaultDeferredFactory.
  */
 class DefaultDeferredFactory extends DeferredFactory
 {
