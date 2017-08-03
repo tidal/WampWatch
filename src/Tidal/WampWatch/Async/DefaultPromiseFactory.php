751a98123f0a5cfc661c9bccf7d408b8b0e89e1d
Timo Michna <timomichna@yahoo.de>
2017-08-03 15:47:34 +0000

    Apply fixes from StyleCI

--- src/Tidal/WampWatch/Async/DefaultPromiseFactory.php
+++ src/Tidal/WampWatch/Async/DefaultPromiseFactory.php
@@ -1,10 +1,12 @@
 <?php
-/**
+
+/*
  * This file is part of the Tidal/WampWatch package.
- *  (c) 2016 Timo Michna <timomichna/yahoo.de>
+ *   (c) 2016 Timo Michna <timomichna/yahoo.de>
  *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
+ *
  */
 
 namespace Tidal\WampWatch\Async;
@@ -12,9 +14,8 @@ namespace Tidal\WampWatch\Async;
 use Tidal\WampWatch\Adapter\React\PromiseFactory;
 
 /**
- * Class Tidal\WampWatch\Async\DefaultPromiseFactory *
+ * Class Tidal\WampWatch\Async\DefaultPromiseFactory *.
  */
 class DefaultPromiseFactory extends PromiseFactory
 {
-
 }
