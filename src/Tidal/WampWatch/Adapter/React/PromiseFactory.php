ee3cf02017b1b31ba7e59cd124ad001722b309a2
Timo Michna <timomichna@yahoo.de>
2017-10-22 00:18:53 +0200

    add promise combinator methods

--- src/Tidal/WampWatch/Adapter/React/PromiseFactory.php
+++ src/Tidal/WampWatch/Adapter/React/PromiseFactory.php
@@ -12,7 +12,10 @@
 namespace Tidal\WampWatch\Adapter\React;
 
 use Tidal\WampWatch\Async\Adapter\PromiseFactoryInterface;
+use React\Promise as ReactCombinators;
 use React\Promise\Promise as ReactPromise;
+use Tidal\WampWatch\Async\PromiseInterface;
+use InvalidArgumentException;
 
 class PromiseFactory implements PromiseFactoryInterface
 {
@@ -40,5 +43,101 @@ class PromiseFactory implements PromiseFactoryInterface
     public function createFromAdaptee($adaptee)
     {
         return new PromiseAdapter($adaptee);
+    }
+
+    /**
+     * @param PromiseInterface[]|ReactPromise[] $promises
+     *
+     * @return PromiseAdapter
+     */
+    public function all(array $promises)
+    {
+        return $this->createFromAdaptee(
+            ReactCombinators\all(
+                self::extractReactPromises(
+                    $promises
+                )
+            )
+        );
+    }
+
+    /**
+     * @param PromiseInterface[]|ReactPromise[] $promises
+     *
+     * @return PromiseAdapter
+     */
+    public function any(array $promises)
+    {
+        return $this->createFromAdaptee(
+            ReactCombinators\all(
+                self::extractReactPromises(
+                    $promises
+                )
+            )
+        );
+    }
+
+    /**
+     * @param PromiseInterface[]|ReactPromise[] $promises
+     * @param int $count
+     *
+     * @return PromiseAdapter
+     */
+    public function some(array $promises, int $count)
+    {
+        return $this->createFromAdaptee(
+            ReactCombinators\some(
+                self::extractReactPromises(
+                    $promises
+                ),
+                $count
+            )
+        );
+    }
+
+    /**
+     * @param PromiseInterface[]|ReactPromise[] $promises
+     *
+     * @return PromiseAdapter
+     */
+    public function first(array $promises)
+    {
+        return $this->createFromAdaptee(
+            ReactCombinators\race(
+                self::extractReactPromises(
+                    $promises
+                )
+            )
+        );
+    }
+
+    /**
+     * @param PromiseInterface[]|ReactPromise[] $promises
+     *
+     * @return ReactPromise[]
+     *
+     * @throws InvalidArgumentException
+     */
+    protected static function extractReactPromises(array $promises)
+    {
+        $results = [];
+        foreach ($promises as $promise) {
+            if (!$promise instanceof PromiseInterface && !$promise instanceof ReactPromise) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        "Promises must be either instance of '%s' or '%s' in %s",
+                        PromiseInterface::class,
+                        ReactPromise::class,
+                        __METHOD__
+                    )
+                );
+            }
+
+            $results[] = $promise instanceof PromiseAdapter
+                ? $promise->getAdaptee()
+                : $promise;
+        }
+
+        return $results;
     }
 }
